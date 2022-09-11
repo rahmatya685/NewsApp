477153fca5ddc8d7abd7596b943cc9ea56d695e5
Alireza Rahmaty <alireza.rahmaty@getyourguide.com>
2022-09-11 13:45:44 +0200

    Fix project build, update dependencies

--- common/repo_impl/src/main/java/com/newsapp/remo_impl/di/module/LocalDbModule.kt
+++ common/repo_impl/src/main/java/com/newsapp/remo_impl/di/module/LocalDbModule.kt
@@ -11,35 +11,35 @@ import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 interface LocalDbModule {
 
-    @get:[Binds Singleton]
-    val BookmarkedStoriesRepoImpl.bookmarkedRepo:BookmarkedStoriesRepo
+  @get:[Binds Singleton]
+  val BookmarkedStoriesRepoImpl.bookmarkedRepo: BookmarkedStoriesRepo
 
-    companion object {
+  companion object {
 
-        @Provides
-        @Singleton
-        fun provideAppDatabase(@ApplicationContext appContext: Context): LocalDb {
-            return Room.databaseBuilder(
-                appContext,
-                LocalDb::class.java,
-                DB_NAME
-            ).build()
-        }
-
-        @Provides
-        fun provideBookmarkedStoriesDao(db: LocalDb): BookmarkedEntityDao {
-            return db.bookmarkedEntityDao()
-        }
-
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext appContext: Context): LocalDb {
+      return Room.databaseBuilder(
+        appContext,
+        LocalDb::class.java,
+        DB_NAME
+      ).build()
     }
+
+    @Provides
+    fun provideBookmarkedStoriesDao(db: LocalDb): BookmarkedEntityDao {
+      return db.bookmarkedEntityDao()
+    }
+
+  }
 
 }
--- common/repo_impl/src/main/java/com/newsapp/remo_impl/di/module/NetModule.kt
+++ common/repo_impl/src/main/java/com/newsapp/remo_impl/di/module/NetModule.kt
@@ -9,11 +9,11 @@ import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 interface NetModule {
 
 
